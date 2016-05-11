f49dc676fd59e41266017470273152f0de498386
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-11 16:26:07 -0300

    changing comments

--- R/adding_subnetwork_id.R
+++ R/adding_subnetwork_id.R
@@ -1,14 +1,13 @@
 #-----------------------------------------------------------------------------------------------------#
 
 # Description: 
-#   Adds a columns to the data frames core_animals and core_plants and that containg a subnetwork 
-#   identifier.
+#   Adds columns to the data frames core_animals and core_plants that containg a subnetwork identifiers.
 #
 # Returns:
 #   Saves 2 csv tables, core_animals_sub and core_plants_sub.
 
+# reading data
 mat = as.matrix(read.table("data/complete_network.txt")) # reading interaction matrix
-
 core_plants = read.csv("data/core_plants.csv") # reading data on plant sp
 names(core_plants) = c("sp", "general_core", "pollination_core", "ant_core", "dispersal_core")
 rownames(mat) == core_plants$sp # checking if sp are in the same order
@@ -17,15 +16,17 @@ core_animals = read.csv("data/core_animals.csv") # reading data on animal sp
 names(core_animals) = c("sp", "general_core", "pollination_core", "ant_core", "dispersal_core")
 colnames(mat) == core_animals$sp # checking if sp are in the same order
 head(core_animals)
+
 # subnetwork animal sp richness: pollination = 173, ants = 30, dispersal = 46
 n_pol = 173
 n_ants = 30
 n_disp = 46
+# adding columns with animal sp subnetwork id
 core_animals$pol_subnetwork = c(rep(1, n_pol), rep(0, nrow(core_animals) - n_pol))
 core_animals$ant_subnetwork = c(rep(0, n_pol), rep(1, n_ants), rep(0, nrow(core_animals) - (n_pol + n_ants)))
 core_animals$disp_subnetwork = c(rep(0, n_pol + n_ants), rep(1, n_disp))
 
-# adding column with plant sp subnetwork id
+# adding columns with plant sp subnetwork id
 # pollination
 sub_mat_pol = mat[ , 1:173]
 plant_not_sub_pol = as.numeric(which(apply(sub_mat_pol, 1, sum) == 0))
