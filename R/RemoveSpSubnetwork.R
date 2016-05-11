38624c9dde8a8cbdb2c67b7f07718c41b283ee9b
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-11 16:53:06 -0300

    adding function that randomly removes species from a subnetwork embedded in a network

---
+++ R/RemoveSpSubnetwork.R
@@ -0,0 +1,19 @@
+#-----------------------------------------------------------------------------------------------------#
+
+RemoveSpSubnetwork = function(s, n, mat, sub_id) {
+  # Performs s simulations of random removal of n species from a subnetwork embedded in a complete 
+  # network.
+  # 
+  # Args:
+  #   s: number of simulations (number of different complete networks that will be generated)
+  #   n: number of species that will be removed
+  #   mat: bipartite matrix with plants in rows and animals in columns
+  #   sub_id: data frame with 2 columns identifying the plants (1st column) and animals 
+  #           (2nd column) that belong to the subnetwork (1: belong; 0: do not belong)
+  #
+  # Returns:
+  #   s complete networks, each with n species removed from the desired subnetwork
+  
+}
+
+#-----------------------------------------------------------------------------------------------------#
