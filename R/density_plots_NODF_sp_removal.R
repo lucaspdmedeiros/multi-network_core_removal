ee78036e21011d65cd0cde244328c6c5eb46fbe8
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-16 13:08:27 -0300

    changed alpha value

--- R/density_plots_NODF_sp_removal.R
+++ R/density_plots_NODF_sp_removal.R
@@ -18,7 +18,7 @@ NODF_random_gen = data.frame(NODF = NODF_gen_core[-c(1, 2), 1]) # nestedness aft
 p_gen = sum(NODF_random_gen < NODF_ref_gen[2, 2])/nrow(NODF_random_gen)
 # plotting
 ggplot(NODF_random_gen, aes(x = NODF)) + 
-  geom_density(alpha = 0.4, size = 0.8, fill = "#B79F00") + # density plot
+  geom_density(alpha = 0.5, size = 0.8, fill = "#B79F00") + # density plot
   geom_vline(data = NODF_ref_gen, # plotting vertical lines
              aes(xintercept = NODF,  colour = type), 
              linetype = "dashed", size = 1) +
@@ -40,7 +40,7 @@ NODF_random_pol = data.frame(NODF = NODF_pol_core[-c(1, 2), 1]) # nestedness aft
 p_pol = sum(NODF_random_pol < NODF_ref_pol[2, 2])/nrow(NODF_random_pol)
 # plotting
 ggplot(NODF_random_pol, aes(x = NODF)) + 
-  geom_density(alpha = 0.4, size = 0.8, fill = "#C77CFF") + # density plot
+  geom_density(alpha = 0.5, size = 0.8, fill = "#C77CFF") + # density plot
   geom_vline(data = NODF_ref_pol, # plotting vertical lines
              aes(xintercept = NODF,  colour = type), 
              linetype = "dashed", size = 1) +
@@ -62,7 +62,7 @@ NODF_random_ant = data.frame(NODF = NODF_ant_core[-c(1, 2), 1]) # nestedness aft
 p_ant = sum(NODF_random_ant < NODF_ref_ant[2, 2])/nrow(NODF_random_ant)
 # plotting
 ggplot(NODF_random_ant, aes(x = NODF)) + 
-  geom_density(alpha = 0.4, size = 0.8, fill = "#F8766D") + # density plot
+  geom_density(alpha = 0.5, size = 0.8, fill = "#F8766D") + # density plot
   geom_vline(data = NODF_ref_ant, # plotting vertical lines
              aes(xintercept = NODF,  colour = type), 
              linetype = "dashed", size = 1) +
@@ -83,7 +83,7 @@ NODF_random_disp = data.frame(NODF = NODF_disp_core[-c(1, 2), 1]) # nestedness a
 p_disp = sum(NODF_random_disp < NODF_ref_disp[2, 2])/nrow(NODF_random_disp)
 # plotting
 ggplot(NODF_random_disp, aes(x = NODF)) + 
-  geom_density(alpha = 0.4, size = 0.8, fill = "#7CAE00") + # density plot
+  geom_density(alpha = 0.5, size = 0.8, fill = "#7CAE00") + # density plot
   geom_vline(data = NODF_ref_disp, # plotting vertical lines
              aes(xintercept = NODF,  colour = type), 
              linetype = "dashed", size = 1) +
