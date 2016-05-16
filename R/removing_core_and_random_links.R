76622c47e174b8661120fb5d89b306ebd50056d3
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-16 19:00:29 -0300

    script to remove core and random links

---
+++ R/removing_core_and_random_links.R
@@ -0,0 +1,81 @@
+#-----------------------------------------------------------------------------------------------------#
+
+# Description:
+#   Removes links between species from the general core and the core of each subnetwork (pollination, 
+#   ant and seed-dispersal) and saves the rarefied interaction matrix. Then, removes the same number 
+#   of core links from the complete network and from each of the subnetworks 100 times for each and 
+#   saves the rarefied interaction matrices.
+# Returns:
+#   Generate and saves 404 rarefied interaction matrices. 
+
+source("R/RemoveLinksSubnetwork.R")
+
+# reading data
+mat = as.matrix(read.table("data/complete_network.txt")) # reading interaction matrix
+links_total = sum(mat)
+core_plants = read.csv("output/data/core_plants_sub.csv") # reading data on plant sp
+core_animals = read.csv("output/data/core_animals_sub.csv") # reading data on animal sp
+
+# general core
+id_plants_gen_core = which(core_plants$general_core == 1)
+id_animals_gen_core = which(core_animals$general_core == 1)
+links_gen_core = sum(mat[id_plants_gen_core, id_animals_gen_core])
+mat_no_gen_core = mat
+mat_no_gen_core[id_plants_gen_core, id_animals_gen_core] = 0
+sum(mat_no_gen_core)
+# save matrix without general core links
+write.table(mat_no_gen_core, 
+            "output/data/core_links_removal/mats_gen_core/net_no_general_core_links.txt",
+            row.names = FALSE, col.names = FALSE)
+# rarefaction simulation
+s = 100
+sub_id_plants = rep(1, nrow(mat)) # defining the subnetwork, namely the complete network
+sub_id_animals = rep(1, ncol(mat)) # defining the subnetwork, namely the complete network
+mat_list = RemoveLinksSubnetwork(s, l = links_gen_core, mat, sub_id_plants, sub_id_animals)
+# save ensemble of rarefied matrices
+for (i in 1:s)
+  write.table(mat_list[[i]], paste("output/data/core_links_removal/mats_gen_core/",
+                                   "net_random_links_general_core_", i, ".txt", sep = ""),
+              row.names = FALSE, col.names = FALSE)
+
+# plant-pollinator subnetwork core
+id_plants_pol_core = which(core_plants$pollination_core == 1)
+id_animals_pol_core = which(core_animals$pollination_core == 1)
+links_pol_core = sum(mat[id_plants_pol_core, id_animals_pol_core])
+mat_no_pol_core = mat
+mat_no_pol_core[id_plants_pol_core, id_animals_pol_core] = 0
+sum(mat_no_pol_core)
+# save matrix without general core links
+write.table(mat_no_pol_core, 
+            "output/data/core_links_removal/mats_pol_core/net_no_pollination_core_links.txt",
+            row.names = FALSE, col.names = FALSE)
+# rarefaction simulation
+s = 100
+sub_id_plants = core_plants$pol_subnetwork # defining the subnetwork
+sub_id_animals = core_animals$pol_subnetwork # defining the subnetwork
+mat_list = RemoveLinksSubnetwork(s, l = links_pol_core, mat, sub_id_plants, sub_id_animals)
+# save ensemble of rarefied matrices
+for (i in 1:s) 
+  write.table(mat_list[[i]], paste("output/data/core_links_removal/mats_pol_core/",
+                                   "net_random_links_pollination_core_", i, ".txt", sep = ""),
+              row.names = FALSE, col.names = FALSE)
+
+
+# plant-ant subnetwork core
+
+# save matrix without plant-ant subnetwork core
+
+# rarefaction simulation
+
+# save ensemble of rarefied matrices
+
+
+# plants-seed-disperser subnetwork core
+
+# save matrix without plant-seed-disperser subnetwork core
+
+# rarefaction simulation
+
+# save ensemble of rarefied matrices
+
+#-----------------------------------------------------------------------------------------------------#
