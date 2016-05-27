552e778128545b4420da0039a77c5f17a6799056
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-27 15:35:46 -0300

    add p value calculations

--- R/nestedness_sp_rarefied_nets.R
+++ R/nestedness_sp_rarefied_nets.R
@@ -3,8 +3,11 @@
 # Description:
 #   Calculates nestedness of species-rarefied interaction matrices regarding general core, plant-pollinator 
 #   subnetwork core, plant-ant subnetwork core, and plants-seed-disperser subnetwork core.
+#   Calculates the p values by comparing networks with core species removed and networks with randomly
+#   removed species.
 # Returns:
-#   Saves tables containing the nestedness values for each of the 4 scenarios described above.
+#   Saves tables containing the nestedness values for each of the 4 scenarios described above and a
+#   table containing observed nestedness values and p values.
 
 library(bipartite)
 
@@ -25,7 +28,9 @@ for (i in 1:100) {
 NODF_gen_core = as.numeric(c(NODF_complete, NODF_no_gen_core, NODF_gen_core_random))
 write.csv(NODF_gen_core, "output/data/core_species_removal/nestedness/NODF_general_core_removal.csv",
           row.names = FALSE)
-  
+# p value
+p_gen = sum(NODF_gen_core[3:length(NODF_gen_core)] < NODF_gen_core[2])/length(NODF_gen_core[3:length(NODF_gen_core)])
+
 # plant-pollinator subnetwork core
 mat_no_pol_core = as.matrix(read.table("output/data/core_species_removal/mats_pol_core/net_no_pollination_core.txt"))
 NODF_no_pol_core = nested(mat_no_pol_core, method = "NODF2")
@@ -39,6 +44,8 @@ for (i in 1:100) {
 NODF_pol_core = as.numeric(c(NODF_complete, NODF_no_pol_core, NODF_pol_core_random))
 write.csv(NODF_pol_core, "output/data/core_species_removal/nestedness/NODF_pollination_core_removal.csv",
           row.names = FALSE)
+# p value
+p_pol = sum(NODF_pol_core[3:length(NODF_pol_core)] < NODF_pol_core[2])/length(NODF_pol_core[3:length(NODF_pol_core)])
 
 # plant-ant subnetwork core
 mat_no_ant_core = as.matrix(read.table("output/data/core_species_removal/mats_ant_core/net_no_ant_core.txt"))
@@ -53,6 +60,8 @@ for (i in 1:100) {
 NODF_ant_core = as.numeric(c(NODF_complete, NODF_no_ant_core, NODF_ant_core_random))
 write.csv(NODF_ant_core, "output/data/core_species_removal/nestedness/NODF_ant_core_removal.csv",
           row.names = FALSE)
+# p value
+p_ant = sum(NODF_ant_core[3:length(NODF_ant_core)] < NODF_ant_core[2])/length(NODF_ant_core[3:length(NODF_ant_core)])
   
 # plants-seed-disperser subnetwork core
 mat_no_disp_core = as.matrix(read.table("output/data/core_species_removal/mats_disp_core/net_no_disp_core.txt"))
@@ -66,6 +75,17 @@ for (i in 1:100) {
 # saving
 NODF_disp_core = as.numeric(c(NODF_complete, NODF_no_disp_core, NODF_disp_core_random))
 write.csv(NODF_disp_core, "output/data/core_species_removal/nestedness/NODF_disp_core_removal.csv",
+          row.names = FALSE)
+# p value
+p_disp = sum(NODF_disp_core[3:length(NODF_disp_core)] < NODF_disp_core[2])/length(NODF_disp_core[3:length(NODF_disp_core)])
+
+# saving table with NODF values and p values
+results_df = data.frame(NODF_complete_net = rep(NODF_gen_core[1], 4), 
+                        NODF_core_removed = c(NODF_gen_core[2], NODF_pol_core[2], 
+                                              NODF_ant_core[2], NODF_disp_core[2]), 
+                        p_value = c(p_gen, p_pol, p_ant, p_disp), 
+                        subnetwork = c("general", "pollination", "ant", "dispersal"))
+write.csv(results_df, file = "output/data/core_species_removal/nestedness/nestedness_p_values.csv", 
           row.names = FALSE)
 
 #-----------------------------------------------------------------------------------------------------#
