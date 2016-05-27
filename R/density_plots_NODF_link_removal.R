e867d0fd1f1107dfa854718ead27ec6296c74b63
Lucas Medeiros <lucaspdmedeiros@gmail.com>
2016-05-27 14:40:07 -0300

    change description

--- R/density_plots_NODF_link_removal.R
+++ R/density_plots_NODF_link_removal.R
@@ -2,7 +2,7 @@
 
 # Description:
 #   Plots the distribution of nestedness values of randomly link-rarefied networks and a reference value:
-#   the nestedness value of the network without the core links of a each subnetwork.
+#   the nestedness value of the network without the core links of each subnetwork.
 # Returns:
 #   Saves one plot as jpeg file.
 
